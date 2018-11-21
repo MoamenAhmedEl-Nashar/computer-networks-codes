9531d12a959bf331bc3e755e24e44401c5f3571b
Omar M. Adel <eng.omaradel95@yahoo.com>
2018-11-21 19:27:23 +0200

    Input to alter changed - Output polynomial added
    In the previous version alter took the remainder (output of generator function) only while it should take the whole transmited message.
    
    The polynomial was required to be in the output file.

--- NetworkAssignment1.cpp
+++ NetworkAssignment1.cpp
@@ -6,89 +6,90 @@
 using namespace std;
 
 /*******************************************************************************
- *                          Functions Prototybes                               *
- *******************************************************************************/
+*                          Functions Prototybes                               *
+*******************************************************************************/
 
-string xor2 (string str1, string str2);
-string generator (string message, string polynomial); // used at sender
-void verify (string message, string polynomial);  //used at reciever
-string alter(int index,string transmitted_message);
+string xor2(string str1, string str2);
+string generator(string message, string polynomial); // used at sender
+void verify(string message, string polynomial);  //used at reciever
+string alter(int index, string transmitted_message);
 
 /*******************************************************************************
- *                             main function                                   *
- *******************************************************************************/
+*                             main function                                   *
+*******************************************************************************/
 
-int main ()
+int main()
 {
 	/*read message and polynomial generator*/
 	int index;
 	ifstream input("input.txt");
 	string message, polynomial, result;
-	input>>message;
-	input>>polynomial;
+	input >> message;
+	input >> polynomial;
 	/*generator(input file)-> verifier case*/
 	ofstream correct("correct_output.txt");
-	result = generator(message, polynomial);  /* get the result of CRC */
-	correct << message+result;
+	result = generator(message, polynomial);  /* get the remainder of CRC */
+	correct << message + result;
 	verify(message + result, polynomial);
-	cout << endl << "transmitted message ---> " << message + result<<"\n";
+	cout << endl << "transmitted message ---> " << message + result << "\n";
 	/*generator(input file)-> alter -> verifier case*/
 	ofstream error("error_output.txt");
 	//The index >=0 && index < length of the message
 	cout << "The index of the element to alter is: ";
-	cin>>index;
-	result = alter(index,generator(message, polynomial));  /* get the result of CRC */
-	error << message+result;
-	verify(message+result,polynomial);
-	cout << endl << "transmitted message ---> " << message + result<<"\n";
-		 
+	cin >> index;
+	result = alter(index, message+generator(message, polynomial));  /* get the altered version of transmitted message */
+	error << result;
+	error << "\n"<<polynomial;
+	verify(result, polynomial);
+	cout << endl << "transmitted message ---> " << result << "\n";
+
 	system("pause");
 	return 0;
 }
 
 /*******************************************************************************
- *                          Functions Definitions                              *
- *******************************************************************************/
+*                          Functions Definitions                              *
+*******************************************************************************/
 
 /*  this function is to calcualte the result of the CRC algorithm */
-string generator (string message, string polynomial)
+string generator(string message, string polynomial)
 {
 	int length = polynomial.length();      /* the length of the generator */
 	string str;
 
-	for(int i=0; i <polynomial.length()-1;i++)  /* add number of zeros = the polynomial degree after the message*/
+	for (int i = 0; i <polynomial.length() - 1; i++)  /* add number of zeros = the polynomial degree after the message*/
 	{
-		message+='0';
+		message += '0';
 	}
 
-	str = message.substr(0,length);     /* get the first number of bits = polynomial length to make them xor with the polynomial */ 
+	str = message.substr(0, length);     /* get the first number of bits = polynomial length to make them xor with the polynomial */
 
-	for(int i = length; i< message.length(); i++)
+	for (int i = length; i< message.length(); i++)
 	{
-		if(str[0] != '0')    /* if the first bit is zero, the str will be XORed with zeros so the output will be the same */
+		if (str[0] != '0')    /* if the first bit is zero, the str will be XORed with zeros so the output will be the same */
 		{                    /* if the first bit is 1, the str will be XORed with the polynomial */
-			str = xor2(str,polynomial);
+			str = xor2(str, polynomial);
 		}
-		str.erase(0,1);     /* delete the first bit as it will be always zero */
-		str+=message[i];    /* get the next bit of message */
+		str.erase(0, 1);     /* delete the first bit as it will be always zero */
+		str += message[i];    /* get the next bit of message */
 	}
 	/* finally there will be another XOR */
-	if(str[0] !='0')   /* if the first bit isn't zero */
+	if (str[0] != '0')   /* if the first bit isn't zero */
 	{                  /* if it zero the str will be the same */
-		str = xor2(str,polynomial);
+		str = xor2(str, polynomial);
 	}
-	str.erase(0,1);  /* delete the first bit as it will be always zero */
+	str.erase(0, 1);  /* delete the first bit as it will be always zero */
 
 	return str;
 }
 
 /* XOR tow strings and get the result,    both strings should be same size */
-string xor2 (string str1, string str2)
+string xor2(string str1, string str2)
 {
 	string result;
-	for(int i=0; i <str1.length(); i++)
+	for (int i = 0; i <str1.length(); i++)
 	{
-		if(str1[i] == str2[i])
+		if (str1[i] == str2[i])
 			result += '0';
 		else
 			result += '1';
@@ -97,18 +98,18 @@ string xor2 (string str1, string str2)
 	return result;
 }
 
-/* print whether the received message is correct or not */ 
-void verify (string message, string polynomial)
+/* print whether the received message is correct or not */
+void verify(string message, string polynomial)
 {
-	if(stoi(generator(message, polynomial))==0)  /* if the result of the CRC is zero, the received message is correct */
-		cout<<endl<<"	message is correct";
+	if (stoi(generator(message, polynomial)) == 0)  /* if the result of the CRC is zero, the received message is correct */
+		cout << endl << "	message is correct";
 	else
-		cout<<endl<<"	message is not correct";
+		cout << endl << "	message is not correct";
 }
 /*change 1 bit from transmitted message (output from generator at sender)*/
-string alter(int index,string transmitted_message)
+string alter(int index, string transmitted_message)
 {
-	string str= transmitted_message;
+	string str = transmitted_message;
 	if (transmitted_message[index] == '0')
 	{
 		str[index] = '1';
